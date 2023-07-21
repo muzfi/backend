4c62ae0f20cc65cdf1309caeca4b94579d251c7c
nish-ahmd-it <ahamedmnn.19@uom.lk>
2023-07-21 23:21:54 +0530

    first commit

---
+++ src/main/java/com/example/muzfi/MuzfiApplication.java
@@ -0,0 +1,13 @@
+package com.example.muzfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzfiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuzfiApplication.class, args);
+    }
+
+}
