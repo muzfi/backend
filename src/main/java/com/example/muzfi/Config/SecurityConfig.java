a66d73456541a70c3b3a0182dc39081233812074
Dileepa97 <54783169+Dileepa97@users.noreply.github.com>
2023-07-27 20:15:25 +0530

    public api end point access

---
+++ src/main/java/com/example/muzfi/Config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.muzfi.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/comments/**").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
