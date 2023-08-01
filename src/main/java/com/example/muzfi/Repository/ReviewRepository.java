2a1398b286c92a37fd943a5c05fe1cd9bf11c149
nish-ahmd-it <ahamedmnn.19@uom.lk>
2023-08-01 16:06:45 +0530

    Added the API's for review

---
+++ src/main/java/com/example/muzfi/Repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.example.muzfi.Repository;
+
+import com.example.muzfi.Model.Review;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, String> {
+    List<Review> findAllByPostId(String postId);
+}
