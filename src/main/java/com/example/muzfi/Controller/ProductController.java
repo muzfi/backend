8cfbad241c895a5c1884d695c46f85c62827dea7
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2023-08-17 10:46:57 +0530

    ProductController updated

--- src/main/java/com/example/muzfi/Controller/ProductController.java
+++ src/main/java/com/example/muzfi/Controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.example.muzfi.Controller;
 
+import com.example.muzfi.Enums.ProductCondition;
 import com.example.muzfi.Model.Product;
 import com.example.muzfi.Services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -35,5 +37,11 @@ public class ProductController {
     public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
         productService.deleteProduct(id);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/byCondition/{condition}")
+    public ResponseEntity<List<Product>> getProductsByCondition(@PathVariable ProductCondition condition) {
+        List<Product> products = productService.getProductsByCondition(condition);
+        return ResponseEntity.ok(products);
     }
 }
