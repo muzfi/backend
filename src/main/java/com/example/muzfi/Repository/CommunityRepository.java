93b1ac9a748a7cca70cfffb153b83036f17b0477
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2023-08-17 10:11:07 +0530

    community Repository part updated

--- src/main/java/com/example/muzfi/Repository/CommunityRepository.java
+++ src/main/java/com/example/muzfi/Repository/CommunityRepository.java
@@ -2,7 +2,12 @@ package com.example.muzfi.Repository;
 
 import com.example.muzfi.Model.Community;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CommunityRepository extends MongoRepository<Community, String> {
     Community findByName(String name);
+    List<Community> findByTypeAndGenre(String type, String genre);
 }
