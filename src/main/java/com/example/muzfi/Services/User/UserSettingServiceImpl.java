8fcfdd5449795ce68ff55d279056f7de36aaeabc
Dileepa97 <54783169+Dileepa97@users.noreply.github.com>
2023-08-30 18:01:49 +0530

    update display lang and content lang

---
+++ src/main/java/com/example/muzfi/Services/User/UserSettingServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.muzfi.Services.User;
+
+import com.example.muzfi.Model.UserSetting;
+import com.example.muzfi.Repository.UserSettingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserSettingServiceImpl implements UserSettingService {
+
+    private final UserSettingRepository userSettingRepository;
+
+    @Autowired
+    public UserSettingServiceImpl(UserSettingRepository userSettingRepository) {
+        this.userSettingRepository = userSettingRepository;
+    }
+
+    @Override
+    public UserSetting createUserSetting(UserSetting setting) {
+        return userSettingRepository.save(setting);
+    }
+
+    @Override
+    public Optional<UserSetting> getUserSettingByUserId(String userid) {
+        return userSettingRepository.findByUserId(userid);
+    }
+
+    @Override
+    public Optional<UserSetting> updateUserSetting(UserSetting setting) {
+        Optional<UserSetting> userSettingOptional = userSettingRepository.findById(setting.getId());
+
+        if (userSettingOptional.isPresent()) {
+            UserSetting existingUserSetting = userSettingOptional.get();
+            existingUserSetting.setDisplayLang(setting.getDisplayLang());
+            existingUserSetting.setContentLang(setting.getContentLang());
+            existingUserSetting.setLastUpdatedDateTime(LocalDateTime.now());
+
+            UserSetting updatedSetting = userSettingRepository.save(existingUserSetting);
+
+            return Optional.of(updatedSetting);
+
+        } else {
+            return Optional.empty();
+        }
+    }
+}
