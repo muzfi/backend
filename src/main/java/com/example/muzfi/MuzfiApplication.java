c12edcae2b790e43774d4256aeab8e4393f6d937
Mareenraj <robinmareen@gmail.com>
2023-11-04 10:14:51 +0530

    update

--- src/main/java/com/example/muzfi/MuzfiApplication.java
+++ src/main/java/com/example/muzfi/MuzfiApplication.java
@@ -1,8 +1,10 @@
 package com.example.muzfi;
 
 import com.example.muzfi.Model.MyBill;
+import com.example.muzfi.Model.OrderDetails;
 import com.example.muzfi.Model.Purchase;
 import com.example.muzfi.Repository.MyBillRepository;
+import com.example.muzfi.Repository.OrderDetailsRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,5 +19,17 @@ public class MuzfiApplication {
     public static void main(String[] args) {
         SpringApplication.run(MuzfiApplication.class, args);
     }
-
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            OrderDetailsRepository orderDetailsRepository
+    ) {
+        return args -> {
+            var orderDetails = OrderDetails.builder()
+                            .OrderId("avndjavjdfkk")
+                                    .orderAmount(7849.0)
+                                            .orderContactNumber("7845944893")
+                                                    .build();
+            orderDetailsRepository.insert(orderDetails);
+        };
+    }
 }
