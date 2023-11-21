569d66f2f5d570e8358de9ba3f0ddf6a4eacd193
Nishad Ahamed <94828211+ahmd-nish@users.noreply.github.com>
2023-11-21 21:37:24 +0530

    Fixed payment portal issues

---
+++ src/main/java/com/example/muzfi/Config/StripeConfig.java
@@ -0,0 +1,19 @@
+package com.example.muzfi.Config;
+
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class StripeConfig {
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    @PostConstruct
+    public void setup() {
+        Stripe.apiKey = stripeApiKey;
+    }
+}
